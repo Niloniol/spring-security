a1dd2a13a413ea1c6bc5bf1574ce07e93e3d633c
Niloniol <hellorock@mail.ru>
2021-03-06 04:14:04 +0700

    Admin role fix

--- src/main/java/web/config/handler/LoginSuccessHandler.java
+++ src/main/java/web/config/handler/LoginSuccessHandler.java
@@ -20,7 +20,7 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
                                         Authentication authentication) throws IOException, ServletException {
         httpServletResponse.sendRedirect("/hello");
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-        if(roles.contains("ADMIN")){
+        if(roles.contains("ROLE_ADMIN")){
             httpServletResponse.sendRedirect("/admin");
         } else if(roles.contains("ROLE_USER")){
             httpServletResponse.sendRedirect("/user");
