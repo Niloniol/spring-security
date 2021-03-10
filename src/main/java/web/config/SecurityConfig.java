620951165bc3edde677420e22728ef9401a75f4a
Niloniol <hellorock@mail.ru>
2021-03-11 04:24:10 +0700

    Bootstrap initialization

--- src/main/java/web/config/SecurityConfig.java
+++ src/main/java/web/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -33,6 +34,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
+    /*@Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("webjars/**");
+    }*/
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
@@ -59,11 +65,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //выклчаем кроссдоменную секьюрность (на этапе обучения неважна)
                 .and().csrf().disable();
 
+
         http
                 // делаем страницу регистрации недоступной для авторизированных пользователей
                 .authorizeRequests()
                 //страницы аутентификаци доступна всем
                 .antMatchers("/login", "/hello", "/registration").permitAll()
+                .antMatchers("webjars/**").permitAll()
                 // защищенные URL
                 .antMatchers("/user").access("hasAnyRole('USER', 'ADMIN')")
                 .antMatchers("/admin/*").access("hasRole('ADMIN')")
